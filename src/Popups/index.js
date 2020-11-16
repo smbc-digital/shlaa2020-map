fed6396d528999a1dd3fb8fc5e64b346cf29f1e4
Dan Kinder <d.kinder02@gmail.com>
2020-11-16 11:40:44 +0000

    Popup Order change

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -3,8 +3,8 @@ const developablesitesPopup = (feature, layer) => {
   const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Map Book Test 3</p>
   <p></p>
   <p class="info">Site Address: ${feature.properties.site_address}</p>
-  <p class="info">Site Area (Ha): ${feature.properties.site_area}</p>
   <p class="info">Site Description: ${feature.properties.site_description_2020}</p>
+  <p class="info">Site Area (Ha): ${feature.properties.site_area}</p>
   <p class="info">Site Capacity: ${feature.properties.capacity}</p>
   <p class="info">Delivery Years: ${feature.properties.delivery_years}</p>
   <p class="info">Assessment: ${feature.properties.assessment_text_2020}</p>
